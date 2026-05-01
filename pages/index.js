f4d517ea0c1686d57057084fbcbf6636b8c7e870
yachmo20 <yachmo20@gmail.com>
2026-05-01 21:08:10 +0900

    Update Home component to fetch and display machines
    Modify Home component to accept machines as props and fetch data from Supabase.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import { supabase } from '../lib/supabase'
 import App from '../machinetool-platform'
 
-export default function Home() {
-  return <App />
+export default function Home({ machines }) {
+  return <App machines={machines} />
+}
+
+export async function getServerSideProps() {
+  const { data: machines, error } = await supabase
+    .from('machines')
+    .select('*')
+    .order('name')
+
+  if (error) {
+    console.error(error)
+    return { props: { machines: [] } }
+  }
+
+  return {
+    props: { machines }
+  }
 }
